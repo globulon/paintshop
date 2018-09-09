6192c3f77059f6ec4e63a85387479f7a223f79e1
Marc-Daniel Ortega <globulon@gmail.com>
2018-09-09 20:38:18 +0100

    Improve parsing test

--- src/test/scala/fp/ParsingTest.scala
+++ src/test/scala/fp/ParsingTest.scala
@@ -5,11 +5,20 @@ import org.scalatest.{MustMatchers, WordSpecLike}
 final class ParsingTest extends WordSpecLike with MustMatchers with Parsing {
   "Parsing" should {
     "successfuly parse preference" in {
-      parse(pref, "1 M").get must be (matte(1))
+      expect(parse(pref, "1 M")) {
+        _ must be (matte(1))
+      }
     }
 
     "successfully parse 1 M 3 G 5 G" in {
-      parse(preferences, "1 M 3 G 5 G").get must be (List(matte(1), gloss(3), gloss(5)))
+      expect(parse(preferences, "1 M 3 G 5 G")) {
+        _ must be (List(matte(1), gloss(3), gloss(5)))
+      }
     }
+  }
+
+  private def expect[A](r: ParseResult[Any])(f: Any ⇒ A): A = r match {
+    case Success(result, _) ⇒ f(result)
+    case _                  ⇒ fail("Expected successful parsing")
   }
 }
